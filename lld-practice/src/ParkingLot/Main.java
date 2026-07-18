a3a69d5e71815117625c2250d4f2987669b9a99f
Ayush Kalra <ayushkalra@Muskans-MacBook-Air.local>
2026-07-18 15:46:52 +0530

    started

---
+++ lld-practice/src/ParkingLot/Main.java
@@ -0,0 +1,8 @@
+package ParkingLot;
+
+public class Main {
+    public static void main(String[] args) {
+        // Your code for this specific question goes here
+        System.out.println("Running Question 1");
+    }
+}
